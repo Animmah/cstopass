eaa5264d87aed51430ccce5f9dec5eb9c19b577d
Aniket Manoj Mahabole <71322677+Animmah@users.noreply.github.com>
2023-06-28 13:47:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: ["https://cstopass.netlify.app"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
