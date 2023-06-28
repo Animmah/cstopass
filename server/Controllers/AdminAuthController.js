921ad196030c096508e9b4b8032e9bf351b9fc56
Aniket Manoj Mahabole <71322677+Animmah@users.noreply.github.com>
2023-06-28 13:48:02 +0530

    Update AdminAuthController.js

--- server/Controllers/AdminAuthController.js
+++ server/Controllers/AdminAuthController.js
@@ -4,27 +4,6 @@ const bcrypt = require("bcryptjs");
 
 //to create users hashing necessary
 
-module.exports.Signup = async (req, res, next) => {
-  try {
-    const { email, password, username, createdAt } = req.body;
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.json({ message: "Account Exists!" });
-    }
-    const user = await User.create({ email, password, username, createdAt });
-    
-    const token = createSecretToken(user._id,"admin");
-    res.cookie("token", token, {
-      withCredentials: true,
-      httpOnly: false,
-    });
-    res.status(201).json({ message: "Admin Created!", success: true, user });
-    // next();
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 module.exports.AdminLogin = async (req, res, next) => {
   try {
     const { email, password } = req.body;
